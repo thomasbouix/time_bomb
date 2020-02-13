fc6103737336c65fba042335d99e50d8b2015be2
3670068 <thomas.bouix@etu.upmc.fr>
2020-02-13 18:21:54 +0100

    fin merge

--- Makefile
+++ Makefile
@@ -4,24 +4,28 @@
 
 CC=g++
 CCFLAGS= -Wall -Werror -std=c++11 -g
-SRC= $(wildcard sources/server/*.cc)
-OBJ= $(SRC:.cc=.o)
-EXEC= timebomb_serv
+LIBFLAGS=-I/usr/include/SDL2 -lSDL2_image -lSDL2_ttf -lSDL2 -lpthread
+SRC_SERV= $(wildcard sources/server/*.cc)
+SRC_CLI= $(wildcard sources/client/*.cc)
+OBJ_SERV= $(SRC_SERV:.cc=.o)
+OBJ_CLI= $(SRC_CLI:.cc=.o)
+EXEC_SERV= timebomb_serv
+EXEC_CLI= timebomb_cli
 
-all: $(EXEC) timebomb_client
+all: $(EXEC_SERV) $(EXEC_CLI) 
 
-timebomb_client :
-	g++ $(CCFLAGS) sources/client/timebomb_cli.cc -o timebomb_cli -lpthread
+$(EXEC_CLI): $(OBJ_CLI)
+	$(CC) $^ -o $@ $(LIBFLAGS)
 
-$(EXEC): $(OBJ)
-	$(CC) $(LIBFLAGS) $^ -o $@ -lpthread
+$(EXEC_SERV): $(OBJ_SERV)
+	$(CC) $^ -o $@ $(LIBFLAGS)
 
 %.o: %.cc
-	$(CC) $(CCFLAGS) -o $@ -c $<
+	$(CC) $(CCFLAGS) -o $@ -c $< $(LIBFLAGS)
 
-.depend:
-	g++ -MM $(SRC) > .depends
--include .depends
+.depend:	
+	g++ -MM $(SRC_SERV) $(SRC_CLI) > .depends
+-include .depends	# inclu le .depends dans le makefile
 
 clean:
-	rm -f $(OBJ) $(EXEC) timebomb_cli
+	rm -f $(OBJ_SERV) $(OBJ_CLI) $(EXEC_SERV) $(EXEC_CLI) 
---
+++ includes/player.hh
@@ -0,0 +1,41 @@
+#pragma once
+#include <vector>
+#include <typeinfo>
+#include "card.hh"
+#include "safety.hh"
+#include "defuser.hh"
+#include "bomb.hh"
+
+#define BLUE 0
+#define RED 1
+
+class Player {
+
+	protected :
+		int color;
+		std::string name;
+		std::vector<Card*> deck; 
+
+	public :
+		Player();								// Instanciation aléatoire, utile pour les BOT
+		Player(int color, std::string name);
+		virtual ~Player();
+
+		std::string get_name(void) const;
+
+		virtual void draw(void) = 0;			// Tirage automatique chez un autre joueur (utile pour BOT ou REAL si temps expiré)
+		void draw(Player * p, int card);		// Tirage ciblé sur un autre joueur spécifié
+
+		void add_card(Card* card);				// Ajoute une carte à son propre deck
+		void remove_card(int card);				// Supprime une carte de son propre deck 
+		void clear_deck();						// Vide son propre deck
+
+		Card* get_card(int card);				// Renvoie un pointeur sur une carte de son propre deck 
+
+		std::string to_string(void);
+		std::string get_color(void);
+
+		int get_size_deck() const;
+
+		std::string get_deck_str(void);			// renvoie un message de protocole de communication pour l'état du deck
+};
